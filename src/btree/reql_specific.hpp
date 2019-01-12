c06ddcb55462f815730cc87df1b71e546b7d076a
Sam Hughes <sam@samuelhughes.com>
2019-01-12 05:20:03 -0800

    Comment about backfilling logic and snapshots

--- src/btree/reql_specific.hpp
+++ src/btree/reql_specific.hpp
@@ -207,6 +207,8 @@ void get_btree_superblock_and_txn_for_writing(
         scoped_ptr_t<real_superblock_t> *got_superblock_out,
         scoped_ptr_t<txn_t> *txn_out);
 
+// TODO: The fact that we have this might mean our backfilling logic is wrong
+// with respect to snapshots.  Take a look.
 void get_btree_superblock_and_txn_for_backfilling(
         cache_conn_t *cache_conn,
         cache_account_t *backfill_account,
