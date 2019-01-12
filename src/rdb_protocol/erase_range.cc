3f7c2cca6b18d052b314a4d1f51c90fa866f8db6
Sam Hughes <sam@samuelhughes.com>
2019-01-12 02:33:33 -0800

    Remove key_modification_callback_t

--- src/rdb_protocol/erase_range.cc
+++ src/rdb_protocol/erase_range.cc
@@ -154,11 +154,9 @@ continue_bool_t rdb_erase_small_range(
                 buf_parent_t(&kv_location.buf), kv_location.value.get());
             // Erase the entry from the leaf node
             kv_location.value.reset();
-            null_key_modification_callback_t null_cb;
             apply_keyvalue_change(&sizer, &kv_location, key.btree_key(),
                                   repli_timestamp_t::invalid /* ignored for erase */,
                                   deletion_context->primary_deleter(),
-                                  &null_cb,
                                   delete_mode_t::ERASE);
 
             // Erase the entry from rocksdb.
