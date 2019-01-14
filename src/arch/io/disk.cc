184d6709abf2e02574bf7d6e8942cec8a712ab71
Sam Hughes <sam@samuelhughes.com>
2019-01-14 15:06:14 -0800

    Removed linux_disk_manager_t and its arch/io/disk stuff

--- src/arch/io/disk.cc
+++ src/arch/io/disk.cc
@@ -13,6 +13,7 @@
 
 #include <unistd.h>
 #include <limits.h>
+#include <stdint.h>
 
 #include <algorithm>
 #include <functional>
@@ -21,192 +22,22 @@
 #include "arch/runtime/thread_pool.hpp"
 #include "arch/runtime/runtime.hpp"
 #include "arch/io/disk/filestat.hpp"
-#include "arch/io/disk/pool.hpp"
-#include "arch/io/disk/conflict_resolving.hpp"
-#include "arch/io/disk/stats.hpp"
-#include "arch/io/disk/accounting.hpp"
 #include "backtrace.hpp"
 #include "config/args.hpp"
 #include "do_on_thread.hpp"
 #include "logger.hpp"
+#include "math.hpp"
 #include "utils.hpp"
 
 
-/* Disk manager object takes care of queueing operations, collecting statistics, preventing
-   conflicts, and actually sending them to the disk. */
-class linux_disk_manager_t : public home_thread_mixin_t {
-public:
-    struct action_t : public stats_diskmgr_t::action_t {
-        action_t(threadnum_t _cb_thread, linux_iocallback_t *_cb)
-            : cb_thread(_cb_thread), cb(_cb) { }
-        threadnum_t cb_thread;
-        linux_iocallback_t *cb;
-    };
-
-
-    linux_disk_manager_t(linux_event_queue_t *queue,
-                         int batch_factor,
-                         int max_concurrent_io_requests,
-                         perfmon_collection_t *stats) :
-        stack_stats(stats, "stack"),
-        conflict_resolver(stats),
-        accounter(batch_factor),
-        backend_stats(stats, "backend", accounter.producer),
-        backend(queue, backend_stats.producer, max_concurrent_io_requests),
-        outstanding_txn(0)
-    {
-        /* Hook up the `submit_fun`s of the parts of the IO stack that are above the
-        queue. (The parts below the queue use the `passive_producer_t` interface instead
-        of a callback function.) */
-        stack_stats.submit_fun = std::bind(&conflict_resolving_diskmgr_t::submit,
-                                           &conflict_resolver, ph::_1);
-        conflict_resolver.submit_fun = std::bind(&accounting_diskmgr_t::submit,
-                                                 &accounter, ph::_1);
-
-        /* Hook up everything's `done_fun`. */
-        backend.done_fun = std::bind(&stats_diskmgr_2_t::done, &backend_stats, ph::_1);
-        backend_stats.done_fun = std::bind(&accounting_diskmgr_t::done, &accounter, ph::_1);
-        accounter.done_fun = std::bind(&conflict_resolving_diskmgr_t::done,
-                                       &conflict_resolver, ph::_1);
-        conflict_resolver.done_fun = std::bind(&stats_diskmgr_t::done, &stack_stats, ph::_1);
-        stack_stats.done_fun = std::bind(&linux_disk_manager_t::done, this, ph::_1);
-    }
-
-    ~linux_disk_manager_t() {
-        rassert(outstanding_txn == 0,
-                "Closing a file with outstanding txns (%" PRIiPTR " of them)\n",
-                outstanding_txn);
-    }
-
-    void *create_account(int pri, int outstanding_requests_limit) {
-        return new accounting_diskmgr_t::account_t(&accounter, pri, outstanding_requests_limit);
-    }
-
-    void destroy_account(void *account) {
-        coro_t::spawn_on_thread([account] {
-            // The account destructor can block if there are outstanding requests.
-            delete static_cast<accounting_diskmgr_t::account_t *>(account);
-        }, home_thread());
-    }
-
-    void submit_action_to_stack_stats(action_t *a) {
-        assert_thread();
-        outstanding_txn++;
-        stack_stats.submit(a);
-    }
-
-    void submit_write(fd_t fd, const void *buf, size_t count, int64_t offset,
-                      void *account, linux_iocallback_t *cb,
-                      bool wrap_in_datasyncs) {
-        threadnum_t calling_thread = get_thread_id();
-
-        action_t *a = new action_t(calling_thread, cb);
-        a->make_write(fd, buf, count, offset, wrap_in_datasyncs);
-        a->account = static_cast<accounting_diskmgr_t::account_t *>(account);
-
-        do_on_thread(home_thread(),
-                     std::bind(&linux_disk_manager_t::submit_action_to_stack_stats, this,
-                               a));
-    }
-
-    void submit_resize(fd_t fd, int64_t old_size, int64_t new_size,
-                      void *account, linux_iocallback_t *cb,
-                      bool wrap_in_datasyncs) {
-        threadnum_t calling_thread = get_thread_id();
-
-        action_t *a = new action_t(calling_thread, cb);
-        a->make_resize(fd, old_size, new_size, wrap_in_datasyncs);
-        a->account = static_cast<accounting_diskmgr_t::account_t *>(account);
-
-        do_on_thread(home_thread(),
-                     std::bind(&linux_disk_manager_t::submit_action_to_stack_stats, this,
-                               a));
-    }
-
-#ifndef USE_WRITEV
-#error "USE_WRITEV not defined.  Did you include pool.hpp?"
-#elif USE_WRITEV
-    void submit_writev(fd_t fd, scoped_array_t<iovec> &&bufs, size_t count,
-                       int64_t offset, void *account, linux_iocallback_t *cb) {
-        threadnum_t calling_thread = get_thread_id();
-
-        action_t *a = new action_t(calling_thread, cb);
-        a->make_writev(fd, std::move(bufs), count, offset);
-        a->account = static_cast<accounting_diskmgr_t::account_t *>(account);
-
-        do_on_thread(home_thread(),
-                     std::bind(&linux_disk_manager_t::submit_action_to_stack_stats, this,
-                               a));
-    }
-#endif  // USE_WRITEV
-
-    void submit_read(fd_t fd, void *buf, size_t count, int64_t offset, void *account, linux_iocallback_t *cb) {
-        threadnum_t calling_thread = get_thread_id();
-
-        action_t *a = new action_t(calling_thread, cb);
-        a->make_read(fd, buf, count, offset);
-        a->account = static_cast<accounting_diskmgr_t::account_t*>(account);
-
-        do_on_thread(home_thread(),
-                     std::bind(&linux_disk_manager_t::submit_action_to_stack_stats, this,
-                               a));
-    }
-
-    void done(stats_diskmgr_t::action_t *a) {
-        assert_thread();
-        outstanding_txn--;
-        action_t *a2 = static_cast<action_t *>(a);
-        bool succeeded = a2->get_succeeded();
-        if (succeeded) {
-            do_on_thread(a2->cb_thread,
-                         std::bind(&linux_iocallback_t::on_io_complete, a2->cb));
-        } else {
-            do_on_thread(a2->cb_thread,
-                         std::bind(&linux_iocallback_t::on_io_failure,
-                                   a2->cb, a2->get_io_errno(),
-                                   static_cast<int64_t>(a2->get_offset()),
-                                   static_cast<int64_t>(a2->get_count())));
-        }
-        delete a2;
-    }
-
-private:
-    /* These fields describe the entire IO stack. At the top level, we allocate a new
-    action_t object for each operation and record its callback. Then it passes through
-    the conflict resolver, which enforces ordering constraints between IO operations by
-    holding back operations that must be run after other, currently-running, operations.
-    Then it goes to the account manager, which queues up running IO operations according
-    to which account they are part of. Finally the "backend" pops the IO operations
-    from the queue.
-
-    At two points in the process--once as soon as it is submitted, and again right
-    as the backend pops it off the queue--its statistics are recorded. The "stack stats"
-    will tell you how many IO operations are queued. The "backend stats" will tell you
-    how long the OS takes to perform the operations. Note that it's not perfect, because
-    it counts operations that have been queued by the backend but not sent to the OS yet
-    as having been sent to the OS. */
-
-    stats_diskmgr_t stack_stats;
-    conflict_resolving_diskmgr_t conflict_resolver;
-    accounting_diskmgr_t accounter;
-    stats_diskmgr_2_t backend_stats;
-    pool_diskmgr_t backend;
-
-
-    intptr_t outstanding_txn;
-
-    DISABLE_COPYING(linux_disk_manager_t);
-};
-
+// TODO: Beware of _direct_io_mode -- we need to pay attention to this when
+// using rockstore (or remove the option).
+// TODO: We don't need the max_concurrent_io_requests option anymore, do we?
 io_backender_t::io_backender_t(rockstore::store *rocks,
                                file_direct_io_mode_t _direct_io_mode,
                                int max_concurrent_io_requests)
     : direct_io_mode(_direct_io_mode),
-      rocks_(rocks),
-      diskmgr(new linux_disk_manager_t(&linux_thread_pool_t::get_thread()->queue,
-                                       DEFAULT_IO_BATCH_FACTOR,
-                                       max_concurrent_io_requests,
-                                       &stats)) { }
+      rocks_(rocks) { }
 
 io_backender_t::~io_backender_t() { }
 
--- src/arch/io/disk.hpp
+++ src/arch/io/disk.hpp
@@ -14,18 +14,13 @@ namespace rockstore {
 class store;
 }
 
+// TODO: Remove these unused(?) options.
 // The maximum concurrent IO requests per event queue.. the default value.
 const int DEFAULT_MAX_CONCURRENT_IO_REQUESTS = 64;
 
 // The maximum user-specifiable value how many concurrent I/O requests may be done per event
 // queue.  (A million is a ridiculously high value, but also safely nowhere near INT_MAX.)
 const int MAXIMUM_MAX_CONCURRENT_IO_REQUESTS = MILLION;
-
-struct iovec;
-
-class linux_iocallback_t;
-
-class linux_disk_manager_t;
 
 class io_backender_t : public home_thread_mixin_debug_only_t {
 public:
@@ -37,14 +32,12 @@ public:
                    int max_concurrent_io_requests = DEFAULT_MAX_CONCURRENT_IO_REQUESTS);
     ~io_backender_t();
     rockstore::store *rocks() { return rocks_; }
-    linux_disk_manager_t *get_diskmgr_ptr() { return diskmgr.get(); }
     file_direct_io_mode_t get_direct_io_mode() const;
 
 protected:
     const file_direct_io_mode_t direct_io_mode;
     rockstore::store *rocks_;
     perfmon_collection_t stats;
-    scoped_ptr_t<linux_disk_manager_t> diskmgr;
 
 private:
     DISABLE_COPYING(io_backender_t);
@@ -53,6 +46,7 @@ private:
 // Makes blocking syscalls.  Upon error, returns the errno value.
 int perform_datasync(fd_t fd);
 
+// TODO: Might we need to fsync the parent of the rockstore directory?
 // Calls fsync() on the parent directory of the given path.
 // Returns the errno value in case of an error and 0 otherwise.
 MUST_USE int fsync_parent_directory(const char *path);
