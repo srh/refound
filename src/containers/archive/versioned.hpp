4c82b4b91b88517e2bcc2007dc03c30eab92f15c
Sam Hughes <sam@samuelhughes.com>
2019-12-19 11:34:05 -0800

    Moved deserialize_cluster_version, reql_version, to cc file.

--- src/containers/archive/versioned.hpp
+++ src/containers/archive/versioned.hpp
@@ -22,58 +22,14 @@ inline void serialize_cluster_version(write_message_t *wm, cluster_version_t v) 
     serialize<cluster_version_t::LATEST_OVERALL>(wm, raw);
 }
 
-inline MUST_USE archive_result_t deserialize_cluster_version(
+MUST_USE archive_result_t deserialize_cluster_version(
         read_stream_t *s,
         cluster_version_t *thing,
-        const std::function<void()> &obsolete_cb) {
-    // Initialize `thing` to *something* because GCC 4.6.3 thinks that `thing`
-    // could be used uninitialized, even when the return value of this function
-    // is checked through `guarantee_deserialization()`.
-    // See https://github.com/rethinkdb/rethinkdb/issues/2640
-    *thing = cluster_version_t::LATEST_OVERALL;
-    int8_t raw;
-    archive_result_t res = deserialize<cluster_version_t::LATEST_OVERALL>(s, &raw);
-    if (bad(res)) { return res; }
-    if (raw == static_cast<int8_t>(obsolete_cluster_version_t::v1_13)
-        || raw == static_cast<int8_t>(obsolete_cluster_version_t::v1_13_2)) {
-        obsolete_cb();
-        crash("Outdated index handling did not crash or throw.");
-    } else {
-        // This is the same rassert in `ARCHIVE_PRIM_MAKE_RANGED_SERIALIZABLE`.
-        if (raw >= static_cast<int8_t>(cluster_version_t::v1_14)
-            && raw <= static_cast<int8_t>(cluster_version_t::v2_5_is_latest)) {
-            *thing = static_cast<cluster_version_t>(raw);
-        } else {
-            throw archive_exc_t{"Unrecognized cluster serialization version."};
-        }
-    }
-    return res;
-}
+        const std::function<void()> &obsolete_cb);
 
-inline MUST_USE archive_result_t deserialize_reql_version(
+MUST_USE archive_result_t deserialize_reql_version(
         read_stream_t *s, reql_version_t *thing,
-        const std::function<void(obsolete_reql_version_t)> &obsolete_cb) {
-    // Initialize `thing` to *something* because GCC 4.6.3 thinks that `thing`
-    // could be used uninitialized, even when the return value of this function
-    // is checked through `guarantee_deserialization()`.
-    // See https://github.com/rethinkdb/rethinkdb/issues/2640
-    *thing = reql_version_t::LATEST;
-    int8_t raw;
-    archive_result_t res = deserialize_universal(s, &raw);
-    if (bad(res)) { return res; }
-    if (raw < static_cast<int8_t>(reql_version_t::EARLIEST)) {
-        guarantee(raw >= static_cast<int8_t>(obsolete_reql_version_t::EARLIEST)
-                  && raw <= static_cast<int8_t>(obsolete_reql_version_t::LATEST));
-        obsolete_cb(static_cast<obsolete_reql_version_t>(raw));
-        crash("Outdated index handling did not crash or throw.");
-    } else {
-        // This is the same rassert in `ARCHIVE_PRIM_MAKE_RANGED_SERIALIZABLE`.
-        guarantee(raw >= static_cast<int8_t>(reql_version_t::EARLIEST)
-                && raw <= static_cast<int8_t>(reql_version_t::LATEST));
-        *thing = static_cast<reql_version_t>(raw);
-    }
-    return res;
-}
+        const std::function<void(obsolete_reql_version_t)> &obsolete_cb);
 
 template <class T>
 void serialize_for_cluster(write_message_t *wm, const T &value) {
