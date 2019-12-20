f9988392f98add0c3daabe97e3fc5b8da40a7885
Sam Hughes <sam@samuelhughes.com>
2019-12-19 18:02:32 -0800

    Remove vestigial deserialization logic of secondary_index_t

--- src/btree/secondary_operations.cc
+++ src/btree/secondary_operations.cc
@@ -17,55 +17,6 @@ RDB_IMPL_SERIALIZABLE_4_SINCE_v2_4(
         secondary_index_t, opaque_definition,
         needs_post_construction_range, being_deleted, id);
 
-// Pre 2.2 we didn't have the `needs_post_construction_range` field, but instead had
-// a boolean `post_construction_complete`.
-// We need to specify a custom deserialization function for that:
-template <cluster_version_t W>
-archive_result_t pre_2_2_deserialize(
-        read_stream_t *s, secondary_index_t *sindex) {
-    archive_result_t res = archive_result_t::SUCCESS;
-    // TODO: We removed sindex->superblock, in fact the block_id_t type entirely, and this
-    // code (which is obsolete deserialization code anyway) should be removed.
-    // res = deserialize<W>(s, deserialize_deref(sindex->superblock));
-    // if (bad(res)) { return res; }
-    res = deserialize<W>(s, deserialize_deref(sindex->opaque_definition));
-    if (bad(res)) { return res; }
-
-    bool post_construction_complete = false;
-    res = deserialize<W>(s, &post_construction_complete);
-    if (bad(res)) { return res; }
-    sindex->needs_post_construction_range =
-        post_construction_complete
-        ? key_range_t::empty()
-        : key_range_t::universe();
-
-    res = deserialize<W>(s, deserialize_deref(sindex->being_deleted));
-    if (bad(res)) { return res; }
-    res = deserialize<W>(s, deserialize_deref(sindex->id));
-    if (bad(res)) { return res; }
-    return res;
-}
-template <> archive_result_t deserialize<cluster_version_t::v1_14>(
-        read_stream_t *s, secondary_index_t *sindex) {
-    return pre_2_2_deserialize<cluster_version_t::v1_14>(s, sindex);
-}
-template <> archive_result_t deserialize<cluster_version_t::v1_15>(
-        read_stream_t *s, secondary_index_t *sindex) {
-    return pre_2_2_deserialize<cluster_version_t::v1_15>(s, sindex);
-}
-template <> archive_result_t deserialize<cluster_version_t::v1_16>(
-        read_stream_t *s, secondary_index_t *sindex) {
-    return pre_2_2_deserialize<cluster_version_t::v1_16>(s, sindex);
-}
-template <> archive_result_t deserialize<cluster_version_t::v2_0>(
-        read_stream_t *s, secondary_index_t *sindex) {
-    return pre_2_2_deserialize<cluster_version_t::v2_0>(s, sindex);
-}
-template <> archive_result_t deserialize<cluster_version_t::v2_1>(
-        read_stream_t *s, secondary_index_t *sindex) {
-    return pre_2_2_deserialize<cluster_version_t::v2_1>(s, sindex);
-}
-
 RDB_IMPL_SERIALIZABLE_2_SINCE_v1_13(sindex_name_t, name, being_deleted);
 
 
