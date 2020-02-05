e1181d001a05314a9b9f297596cee0808cdae449
Sam Hughes <sam@samuelhughes.com>
2020-02-04 21:08:07 -0800

    Fdb-ized get_write_hook (but not set_write_hook)

--- src/rdb_protocol/terms/write_hook.cc
+++ src/rdb_protocol/terms/write_hook.cc
@@ -4,13 +4,16 @@
 #include <string>
 
 #include "clustering/administration/admin_op_exc.hpp"
+#include "clustering/administration/artificial_reql_cluster_interface.hpp"
+#include "clustering/administration/auth/user_fut.hpp"
 #include "containers/archive/string_stream.hpp"
+#include "fdb/retry_loop.hpp"
 #include "rdb_protocol/btree.hpp"
 #include "rdb_protocol/error.hpp"
 #include "rdb_protocol/func.hpp"
+#include "rdb_protocol/minidriver.hpp"
 #include "rdb_protocol/op.hpp"
 #include "rdb_protocol/real_table.hpp"
-#include "rdb_protocol/minidriver.hpp"
 #include "rdb_protocol/term_walker.hpp"
 #include "rdb_protocol/terms/terms.hpp"
 
@@ -23,18 +26,18 @@ std::string format_write_hook_query(const write_hook_config_t &config) {
 
 namespace ql {
 
-// QQQ: Fdb-ize both these functions, and make use of write hooks.
-class set_write_hook_term_t : public op_term_t {
+// OOO: Fdb-ize this function.
+class set_write_hook_term_t final : public op_term_t {
 public:
     set_write_hook_term_t(compile_env_t *env, const raw_term_t &term)
         : op_term_t(env, term, argspec_t(2)) { }
 
-    deterministic_t is_deterministic() const final {
+    deterministic_t is_deterministic() const override {
         return deterministic_t::no();
     }
 
-    virtual scoped_ptr_t<val_t> eval_impl(
-        scope_env_t *env, args_t *args, eval_flags_t) const {
+    scoped_ptr_t<val_t> eval_impl(
+        scope_env_t *env, args_t *args, eval_flags_t) const override {
         counted_t<table_t> table = args->arg(env, 0)->as_table(env->env);
 
         bool existed = false;
@@ -135,40 +138,51 @@ public:
         return new_val(std::move(res).to_datum());
     }
 
-    virtual const char *name() const { return "set_write_hook"; }
+    const char *name() const override { return "set_write_hook"; }
 };
 
-class get_write_hook_term_t : public op_term_t {
+class get_write_hook_term_t final : public op_term_t {
 public:
     get_write_hook_term_t(compile_env_t *env, const raw_term_t &term)
         : op_term_t(env, term, argspec_t(1)) { }
 
-    deterministic_t is_deterministic() const final {
+    deterministic_t is_deterministic() const override {
         return deterministic_t::no();
     }
 
-    virtual scoped_ptr_t<val_t> eval_impl(scope_env_t *env, args_t *args, eval_flags_t) const {
+    scoped_ptr_t<val_t> eval_impl(scope_env_t *env, args_t *args, eval_flags_t) const override {
         counted_t<table_t> table = args->arg(env, 0)->as_table(env->env);
 
-        datum_t write_hook;
+        if (table->db->name == artificial_reql_cluster_interface_t::database_name) {
+            return new_val(datum_t::null());
+        }
+
+        table_config_t table_config;
         try {
-            admin_err_t error;
-            if (!env->env->reql_cluster_interface()->get_write_hook(
-                    env->env->get_user_context(),
-                    table->db,
-                    name_string_t::guarantee_valid(table->name.c_str()),
-                    env->env->interruptor,
-                    &write_hook,
-                    &error)) {
-                REQL_RETHROW(error);
-            }
+            fdb_error_t loop_err = txn_retry_loop_coro(env->env->get_rdb_ctx()->fdb, env->env->interruptor, [&](FDBTransaction *txn) {
+                // TODO: Read-only txn.
+                auth::fdb_user_fut<auth::db_table_config_permission> auth_fut
+                    = env->env->get_user_context().transaction_require_db_and_table_config_permission(
+                        txn, table->db->id, table->get_id());
+
+                table_config_t cfg = config_cache_get_table_config(txn,
+                    table->tbl->cv.get(),
+                    table->get_id(),
+                    env->env->interruptor);
+
+                auth_fut.block_and_check(env->env->interruptor);
+
+                table_config = std::move(cfg);
+            });
+            guarantee_fdb_TODO(loop_err, "retry loop fali in get_write_hook_term");
         } catch (auth::permission_error_t const &permission_error) {
             rfail(ql::base_exc_t::PERMISSION_ERROR, "%s", permission_error.what());
         }
+        datum_t write_hook = convert_write_hook_to_datum(table_config.write_hook);
         return new_val(std::move(write_hook));
     }
 
-    virtual const char *name() const { return "get_write_hook"; }
+    const char *name() const override { return "get_write_hook"; }
 };
 
 counted_t<term_t> make_set_write_hook_term(
