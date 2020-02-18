0c749d70230806aad52ae9bc34e94e3ded4629aa
Sam Hughes <sam@samuelhughes.com>
2020-02-18 03:05:35 -0800

    Some misc RDB_CF cleanup

--- src/protocol_api.cc
+++ src/protocol_api.cc
@@ -3,15 +3,3 @@
 #include "arch/runtime/coroutines.hpp"
 
 RDB_IMPL_SERIALIZABLE_2_FOR_CLUSTER(cannot_perform_query_exc_t, message, query_state);
-
-#if RDB_CF
-namespace_interface_access_t::namespace_interface_access_t() {}
-
-namespace_interface_access_t::namespace_interface_access_t(
-        const namespace_id_t &table_id) :
-    nsi_(std::make_shared<table_query_client_t>(table_id))
-{
-}
-
-namespace_interface_access_t::~namespace_interface_access_t() {}
-#endif  // RDB_CF
