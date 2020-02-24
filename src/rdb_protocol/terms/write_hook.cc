9f569517c8f580785237d0f2e4a6af6c340f0380
Sam Hughes <sam@samuelhughes.com>
2020-02-24 13:58:55 -0800

    Use prov table in get_write_hook_term_t

--- src/rdb_protocol/terms/write_hook.cc
+++ src/rdb_protocol/terms/write_hook.cc
@@ -162,9 +162,13 @@ public:
     }
 
     scoped_ptr_t<val_t> eval_impl(scope_env_t *env, args_t *args, eval_flags_t) const override {
-        counted_t<table_t> table = args->arg(env, 0)->as_table(env->env);
+        provisional_table_id table = args->arg(env, 0)->as_prov_table(env->env);
 
-        if (table->db->name == artificial_reql_cluster_interface_t::database_name) {
+        if (table.prov_db.db_name == artificial_reql_cluster_interface_t::database_name) {
+            if (!artificial_reql_cluster_interface_t::get_table_id(table.table_name).has_value()) {
+                rfail_prov_table_dne(table);
+            }
+
             return new_val(datum_t::null());
         }
 
@@ -172,18 +176,20 @@ public:
         try {
             fdb_error_t loop_err = txn_retry_loop_coro(env->env->get_rdb_ctx()->fdb, env->env->interruptor, [&](FDBTransaction *txn) {
                 // TODO: Read-only txn.
+                std::pair<namespace_id_t, table_config_t> info
+                    = expect_retrieve_table(txn, table, env->env->interruptor).ci_value;
+
+                // We could save a round trip -- the table_config_t fut in
+                // expect_retrieve_table and the auth fut here could be done
+                // simultaneously.
+
                 auth::fdb_user_fut<auth::db_table_config_permission> auth_fut
                     = env->env->get_user_context().transaction_require_db_and_table_config_permission(
-                        txn, table->db->id, table->get_id());
-
-                table_config_t cfg = config_cache_get_table_config(txn,
-                    table->tbl->cv.assert_nonempty(),
-                    table->get_id(),
-                    env->env->interruptor);
+                        txn, info.second.basic.database, info.first);
 
                 auth_fut.block_and_check(env->env->interruptor);
 
-                table_config = std::move(cfg);
+                table_config = std::move(info.second);
             });
             guarantee_fdb_TODO(loop_err, "retry loop fali in get_write_hook_term");
         } catch (auth::permission_error_t const &permission_error) {
