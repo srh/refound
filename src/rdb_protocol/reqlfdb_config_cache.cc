9af3fcaa69fe3e042d9a37a640acca8311eb10fa
Sam Hughes <sam@samuelhughes.com>
2020-08-01 23:14:35 -0700

    Remove comment

--- src/rdb_protocol/reqlfdb_config_cache.cc
+++ src/rdb_protocol/reqlfdb_config_cache.cc
@@ -190,7 +190,6 @@ reqlfdb_config_cache::try_lookup_cached_table(
     return ret;
 }
 
-// NNN: Is this used anymore?
 config_info<optional<database_id_t>>
 config_cache_retrieve_db_by_name(
         const reqlfdb_config_version config_cache_cv,
