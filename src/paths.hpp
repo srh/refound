7080a7ec94a2ac1a80818ace0438b4751ba57c23
Sam Hughes <sam@samuelhughes.com>
2022-08-11 11:01:11 -0700

    Removed TEMPORARY_DIRECTORY_NAME

--- src/paths.hpp
+++ src/paths.hpp
@@ -26,8 +26,6 @@ private:
     std::string path_;
 };
 
-static const char *TEMPORARY_DIRECTORY_NAME = "tmp";
-
 void recreate_temporary_directory(const base_path_t& base_path);
 
 void remove_directory_recursive(const char *path);
